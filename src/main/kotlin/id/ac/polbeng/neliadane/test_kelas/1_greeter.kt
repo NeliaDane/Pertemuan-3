3f784664666a7544b2f96f89b25f58b85d696ed5
[Nelia Dane] <neliadane17@gmail.com>
2022-03-24 12:05:02 +0700

    class dan objek

---
+++ src/main/kotlin/id/ac/polbeng/neliadane/test_kelas/1_greeter.kt
@@ -0,0 +1,11 @@
+package id.ac.polbeng.neliadane.test_kelas
+
+class `1_greeter` {
+    fun greet() {
+        println("Hello object world!")
+    }
+}
+/*
+class yang di beri nama 1_greeter ini terdapat fungsi dari greeter
+yang nantinya akan mencetak baris yang berisi Hello object world!.
+ */
